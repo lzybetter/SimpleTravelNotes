dacf445c221c7ac7cd2e431b1028f94d2321eb31
unknown <>
2016-04-13 08:13:01 +0800

    完成定位类。

---
+++ app/src/main/java/com/lzybetter/simpletravlenotes/LocationNow.java
@@ -0,0 +1,79 @@
+package com.lzybetter.simpletravlenotes;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+/**
+ * Created by lzybetter on 2016/4/12.
+ */
+public class LocationNow {
+
+    private double latitude, longitude;
+    private double[] locationDouble = new double[2];
+    private int test;
+    private String provider;
+
+
+    public double[] LocationNow(Activity activity){
+
+        LocationManager locationManager = (LocationManager)activity.getSystemService(Context.LOCATION_SERVICE);
+        List<String> providerList= locationManager.getProviders(true);
+        if(providerList.contains(LocationManager.NETWORK_PROVIDER)){
+            provider = LocationManager.NETWORK_PROVIDER;
+        }else if(providerList.contains(LocationManager.GPS_PROVIDER)){
+            provider = LocationManager.GPS_PROVIDER;
+        }else {
+            Toast.makeText(activity.getBaseContext(), "未开启定位", Toast.LENGTH_SHORT).show();
+            locationDouble[0] = 0;
+            locationDouble[1] = 0;
+            return locationDouble;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+        if(location != null){
+            latitude = getLatitude(location);
+            longitude = getLongitude(location);
+        }
+        locationManager.requestLocationUpdates(provider, 5000, 1, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location){
+                latitude = getLatitude(location);
+                longitude = getLongitude(location);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        });
+        locationDouble[0] = latitude;
+        locationDouble[1] = longitude;
+        return locationDouble;
+    }
+
+    private double getLatitude(Location location){
+        return location.getLatitude();
+    }
+
+    private double getLongitude(Location location){
+        return location.getLongitude();
+    }
+
+
+}
--- app/src/main/java/com/lzybetter/simpletravlenotes/MainActivity.java
+++ app/src/main/java/com/lzybetter/simpletravlenotes/MainActivity.java
@@ -1,13 +1,38 @@
 package com.lzybetter.simpletravlenotes;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.textView);
+        button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LocationNow locationNow = new LocationNow();
+                double[] location = locationNow.LocationNow(MainActivity.this);
+                double latitude = location[0];
+                double longitude = location[1];
+                if( latitude == 0 && longitude == 0){
+                    textView.setText("未开启定位");
+                }else{
+                    textView.setText("现在的坐标是: " + latitude + " " + longitude);}
+            }
+        });
+
     }
 }
